b7f793a8eccb96d2ec542c8f97d1a8737534839a
pyunghun <564564q@naver.com>
2024-04-01 15:57:16 +0900

    aa

--- src/main/java/com/ohgiraffers/section06/ternary/Application01.java
+++ src/main/java/com/ohgiraffers/section06/ternary/Application01.java
@@ -24,6 +24,7 @@ public class Application01 {
         System.out.println("result2 = " + result2);
         System.out.println("result2 = " + result2);
         System.out.println("result2 = " + result2);
+        System.out.println("result2 = " + result2);
 
 
 
