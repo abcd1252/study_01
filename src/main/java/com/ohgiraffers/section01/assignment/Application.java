a959ffcaf32a93fc4af63e797a7137cec1170c7f
pyunghun <564564q@naver.com>
2024-06-02 16:47:04 +0900

    aa2

--- src/main/java/com/ohgiraffers/section01/assignment/Application.java
+++ src/main/java/com/ohgiraffers/section01/assignment/Application.java
@@ -53,6 +53,7 @@ public class Application {
         System.out.println("num = " + num);
         System.out.println("num = " + num);
         System.out.println("num = " + num);
+        System.out.println("num = " + num);
 
         System.out.println("num = " + num);
 
